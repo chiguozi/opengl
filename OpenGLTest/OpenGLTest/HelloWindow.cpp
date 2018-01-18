6bedd4ffd8eb9193d70a78d8532221498176db1d
chiguozi <qdlzr456@163.com>
2018-01-18 19:19:00 +0800

    hellowindow

--- OpenGLTest/OpenGLTest/HelloWindow.cpp
+++ OpenGLTest/OpenGLTest/HelloWindow.cpp
@@ -6,10 +6,13 @@ void processInput(GLFWwindow *window);
 
 int main()
 {
+	//GLGW初始化
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 此行用来给Mac OS X系统做兼容
 
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn", NULL, NULL);
 	if (window == NULL)
@@ -29,6 +32,8 @@ int main()
 
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+
+	//函数在我们每次循环的开始前检查一次GLFW是否被要求退出，如果是的话该函数返回true然后游戏循环便结束了，之后为我们就可以关闭应用程序了
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
@@ -36,7 +41,9 @@ int main()
 		glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
 		glfwSwapBuffers(window);
+		//函数检查有没有触发什么事件（比如键盘输入、鼠标移动等），然后调用对应的回调函数（可以通过回调方法手动设置）。我们一般在游戏循环的开始调用事件处理函数。
 		glfwPollEvents();
 	}
 
