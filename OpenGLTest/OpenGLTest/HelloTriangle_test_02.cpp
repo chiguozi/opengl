af62ba386aa0096dbf462c3a8467eeda50e539ac
chiguozi <qdlzr456@163.com>
2018-01-25 00:07:24 +0800

    triangle

--- OpenGLTest/OpenGLTest/HelloTriangle_test_02.cpp
+++ OpenGLTest/OpenGLTest/HelloTriangle_test_02.cpp
@@ -2,20 +2,20 @@
 #include "GLFW/glfw3.h"
 #include <iostream>
 
-const GLchar* vertexShaderSource = "#version 330 core\n"
+const GLchar* vertexShaderSource_test_02 = "#version 330 core\n"
 "layout (location = 0) in vec3 position;\n"
 "void main()\n"
 "{\n"
 "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
 "}\0";
-const GLchar* fragmentShaderSource = "#version 330 core\n"
+const GLchar* fragmentShaderSource_test_02 = "#version 330 core\n"
 "out vec4 color;\n"
 "void main()\n"
 "{\n"
 "color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 "}\n\0";
 
-int main()
+int main_test_02()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -42,12 +42,12 @@ int main()
 	glViewport(0, 0, 800, 600);
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &vertexShaderSource_test_02, NULL);
 	glCompileShader(vertexShader);
 
 
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource_test_02, NULL);
 	glCompileShader(fragmentShader);
 
 	GLuint shaderProgram = glCreateProgram();
@@ -58,4 +58,55 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
+	GLfloat vertices1[] =
+	{
+		0.5f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f, //
+	};
+	GLfloat vertices2[] =
+	{
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f,
+	};
+
+	GLuint VAO[2],VBO[2];
+	glGenBuffers(2, VBO);
+	glGenVertexArrays(2, VAO);
+	glBindVertexArray(VAO[0]);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindVertexArray(VAO[1]);
+
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+	//设置顶点属性指针
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	while (!glfwWindowShouldClose(window))
+	{
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO[0]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(VAO[1]);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//函数会交换颜色缓冲（它是一个储存着GLFW窗口每一个像素颜色的大缓冲），它在这一迭代中被用来绘制，并且将会作为输出显示在屏幕上。
+		glfwSwapBuffers(window);
+		//函数检查有没有触发什么事件（比如键盘输入、鼠标移动等），然后调用对应的回调函数（可以通过回调方法手动设置）。我们一般在游戏循环的开始调用事件处理函数。
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
+	return 0;
 }
