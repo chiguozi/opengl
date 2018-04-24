d78abee3288b1b572a24e3ef63ba6fec90ba9906
chiguozi <qdlzr456@163.com>
2018-04-24 08:48:42 +0800

    light 2-3

---
+++ OpenGLTest/OpenGLTest/lighting_02/light_2_3/light_2_3/material.cpp
@@ -0,0 +1,12 @@
+#include "Camera.h"
+#include "Tool.h"
+#include "Shader.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+int main()
+{
+
+	return 0;
+}
